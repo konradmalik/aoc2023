8abebaac45d5314c6a366e729d03d6ec5182de9f
Konrad Malik <konrad.malik@gmail.com>
2023-12-03 19:51:14 +0100

    day 3 part 2

--- 3/main.c
+++ 3/main.c
@@ -18,6 +18,8 @@ struct part {
   // exclusive
   int iend;
   int value;
+  int gear_index;
+  int gear_line;
 };
 
 struct part parse_part_number(char *line, int starti) {
@@ -40,77 +42,56 @@ struct part parse_part_number(char *line, int starti) {
   }
 
   if (consecutive == 0) {
-    struct part p = {-1, -1, -1};
+    struct part p = {-1, -1, -1, -1, -1};
     return p;
   }
   struct part p = {i - strlen(number), i, atoi(number)};
   return p;
 }
 
-int is_symbol(char c) {
-  if (c == '!')
-    return 1;
-  if (c == '@')
-    return 1;
-  if (c == '#')
-    return 1;
-  if (c == '$')
-    return 1;
-  if (c == '%')
-    return 1;
-  if (c == '^')
-    return 1;
-  if (c == '&')
-    return 1;
+int is_gear(char c) {
   if (c == '*')
-    return 1;
-  if (c == '-')
-    return 1;
-  if (c == '_')
-    return 1;
-  if (c == '=')
-    return 1;
-  if (c == '+')
-    return 1;
-  if (c == '?')
-    return 1;
-  if (c == '>')
-    return 1;
-  if (c == '<')
-    return 1;
-  if (c == '/')
     return 1;
   return 0;
 }
 
-int is_part_number(struct part p, char *pline, char *cline, char *nline) {
-  int xmin = max(0, p.istart - 1);
-  int xmax = min(strlen(nline), p.iend + 1);
+int is_potential_gear(int ln, struct part *p, char *pline, char *cline,
+                      char *nline) {
+  int xmin = max(0, p->istart - 1);
+  int xmax = min(strlen(nline), p->iend + 1);
 
   for (int i = xmin; i < xmax; i++) {
     char c = pline[i];
     // printf("checking symbol %c for part %d\n", c, p.value);
-    if (is_symbol(c) > 0) {
+    if (is_gear(c) > 0) {
+      p->gear_line = ln - 1;
+      p->gear_index = i;
       return 1;
     }
   }
 
   char c = cline[xmin];
   // printf("checking symbol %c for part %d\n", c, p.value);
-  if (is_symbol(c) > 0) {
+  if (is_gear(c) > 0) {
+    p->gear_line = ln;
+    p->gear_index = xmin;
     return 1;
   }
 
   c = cline[xmax - 1];
   // printf("checking symbol %c for part %d\n", c, p.value);
-  if (is_symbol(c) > 0) {
+  if (is_gear(c) > 0) {
+    p->gear_line = ln;
+    p->gear_index = xmax - 1;
     return 1;
   }
 
   for (int i = xmin; i < xmax; i++) {
     char c = nline[i];
     // printf("checking symbol %c for part %d\n", c, p.value);
-    if (is_symbol(c) > 0) {
+    if (is_gear(c) > 0) {
+      p->gear_line = ln + 1;
+      p->gear_index = i;
       return 1;
     }
   }
@@ -118,32 +99,32 @@ int is_part_number(struct part p, char *pline, char *cline, char *nline) {
   return 0;
 }
 
-int parse_line(char *l1, char *l2, char *l3) {
+int parse_line(int ln, char *l1, char *l2, char *l3, struct part *parts,
+               int iparts) {
   printf("1: %s", l1);
   printf("2: %s", l2);
   printf("3: %s", l3);
   printf("\n");
 
-  int line_parts_sum = 0;
-
   struct part p = {-1, -1, -1};
   int starti = 0;
   do {
     p = parse_part_number(l2, starti);
-    printf("number start: %d, end: %d, value: %d\n", p.istart, p.iend, p.value);
+    // printf("number start: %d, end: %d, value: %d\n", p.istart, p.iend,
+    // p.value);
     if (p.iend > -1) {
       starti = p.iend;
 
-      if (is_part_number(p, l1, l2, l3) > 0) {
-        printf("%d is part number\n", p.value);
-        line_parts_sum += p.value;
-      } else {
-        printf("%d is NOT part number\n", p.value);
+      if (is_potential_gear(ln, &p, l1, l2, l3) > 0) {
+        printf("%d is a potential gear, gear placement: %d, %d\n", p.value,
+               p.gear_line, p.gear_index);
+        parts[iparts] = p;
+        iparts++;
       }
     }
   } while (p.value != -1);
 
-  return line_parts_sum;
+  return iparts;
 }
 
 int main(void) {
@@ -158,6 +139,8 @@ int main(void) {
   unsigned sum = 0;
   char lines[3][4096];
   int i = -1;
+  struct part parts[4096];
+  int ipart = 0;
   while ((read = getline(&line, &len, fp))) {
     i++;
     printf("\nline %d, read: %ld \n", i, read);
@@ -184,13 +167,28 @@ int main(void) {
       lines[2][l] = '\0';
     }
 
-    sum += parse_line(lines[0], lines[1], lines[2]);
+    ipart = parse_line(i, lines[0], lines[1], lines[2], parts, ipart);
 
     if (read == -1)
       break;
   }
 
-  printf("sum of parts is: %d\n", sum);
+  printf("got potential gears: %d\n", ipart);
+  int gears[400][400];
+  memset(gears, -1, sizeof(gears));
+  for (int i = 0; i < ipart; i++) {
+    struct part p = parts[i];
+    int existing_part = gears[p.gear_line][p.gear_index];
+    if (existing_part != -1) {
+      printf("got actual gear: %d and %d\n", parts[existing_part].value,
+             p.value);
+      sum += parts[existing_part].value * p.value;
+    } else {
+      gears[p.gear_line][p.gear_index] = i;
+    }
+  }
+
+  printf("sum of gear ratios is %d\n", sum);
 
   fclose(fp);
   if (line)
